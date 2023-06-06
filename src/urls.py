e4ed18958423b7788b61e9089e38278f2f82ce7e
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-06 16:35:43 +0600

    implemented bran route

--- src/urls.py
+++ src/urls.py
@@ -22,6 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register(r"category", viewset=views.CategoryViewSet)
+router.register(r"brand", viewset=views.BrandViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
