88643c6f8e97e07a3fec190219e4cfa21a750920
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-08 18:16:17 +0600

    reverse rel

--- src/product/models.py
+++ src/product/models.py
@@ -40,8 +40,10 @@ class ProductLine(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     sku = models.CharField(max_length=100)
     stock_quantity = models.IntegerField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_line"
+    )
     is_active = models.BooleanField(default=False)
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.sku
--- src/product/serializers.py
+++ src/product/serializers.py
@@ -11,21 +11,23 @@ class CategorySerializer(serializers.ModelSerializer):
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = "__all__"
+        # fields = "__all__"
+        exclude = ["id"]
+
+
+class ProductLineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductLine
+        exclude = ["id"]
+        # fields = "__all__"
 
 
 class ProductSerializer(serializers.ModelSerializer):
     brand = BrandSerializer()  # Note: Check Product Model to Know the Name
     category = CategorySerializer()
+    product_line = ProductLineSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = "__all__"
-
-
-class ProductLineSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
-
-    class Meta:
-        model = ProductLine
-        fields = "__all__"
+        exclude = ("id",)
+        # fields = "__all__"
