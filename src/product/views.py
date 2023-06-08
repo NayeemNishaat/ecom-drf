f1eab18f8bd65cf6b38bf30bbcef6d5ff6fb3dc7
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-08 15:10:50 +0600

    filter product by category

--- src/product/views.py
+++ src/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Brand, Category, Product
 from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 from drf_spectacular.utils import extend_schema
@@ -25,10 +26,27 @@ class BrandViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
 
-class ProductViewSet(viewsets.ViewSet):
+class ProductViewSet(
+    viewsets.ViewSet
+):  # ModelViewSet will create all CRUD APIs and ViewSet will only create the get API
     queryset = Product.objects.all()
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
+        """ViewSet for viewing all products"""
+
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"(?P<category>\w+)/all",
+    )
+    def list_by_category(self, request, category=None):
+        """ViewSet for getting products by category"""
+
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
         return Response(serializer.data)
